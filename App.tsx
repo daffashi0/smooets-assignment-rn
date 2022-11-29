3044d06f8f970a65ff11c682957776daec923f27
Daffa Shidiq <daffashi00@gmail.com>
2022-11-29 20:38:27 +0700

    fix config

--- App.tsx
+++ App.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
 
 import {NavigationContainer} from '@react-navigation/native';
-import LoginScreen from './src/screens/Login';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {StrictMode} from 'react';
+import {Provider as ReduxProvider} from 'react-redux';
+import LoginScreen from './src/screens/Login';
 import RegisterScreen from './src/screens/Register';
+import store from './src/store';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Register" component={RegisterScreen} />
-        <Stack.Screen name="Login" component={LoginScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <StrictMode>
+      <ReduxProvider store={store}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Register" component={RegisterScreen} />
+            <Stack.Screen name="Login" component={LoginScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </ReduxProvider>
+    </StrictMode>
   );
 };
 
--- index.js
+++ index.js
@@ -2,17 +2,8 @@
  * @format
  */
 
-import {StrictMode} from 'react';
-import {Provider} from 'react-redux';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import store from './src/store';
 
-AppRegistry.registerComponent(appName, () => (
-  <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </StrictMode>
-));
+AppRegistry.registerComponent(appName, () => App);
